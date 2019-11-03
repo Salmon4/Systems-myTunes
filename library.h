16378c0f4fb5d27fcfa6b36bbc48930ddaa253ea
mmiah00 <mmiah00@stuy.edu>
2019-11-03 03:13:38 +0000

    headers

---
+++ library.h
@@ -0,0 +1,9 @@
+void add_node (struct song_node * l [], struct song_node * adding);
+struct song_node * search_song (struct song_node * l[], struct song_node * target);
+struct song_node * search_artist (struct song_node *l[], char writer []);
+void print_letter (struct song_node *l[], char c);
+void print_artist (struct song_node *l[], char writer[]);
+void printlibrary (struct song_node *l[]);
+void shuffle (struct song_node *l[]);
+void delete (struct song_node *l[], struct song_node *target);
+void clear (struct song_node *l[]); 
