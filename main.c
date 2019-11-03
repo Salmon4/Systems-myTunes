fc54320e9a039035254e333f3053887499e5138d
Brandon <bchen00@stuy.edu>
2019-11-03 01:14:30 +0100

    better test formatting

--- main.c
+++ main.c
@@ -8,19 +8,30 @@ int main () {
   struct song_node * test;
   print_list (test);
   printf("\nAdding to list: \n");
+  printf("Inserting moo by cow\n");
   test = insert_front(test, "moo", "cow");
   print_list(test);
   printf("\n");
+  printf("Inserting woof by dog\n");
   test = insert_front(test, "woof", "dog");
   print_list(test);
   printf("\n");
+  printf("Inserting quack by duck\n");
   test = insert_front(test, "quack", "duck");
   print_list(test);
   printf("\n");
+  printf("Inserting meow by cat\n");
   test = insert_front(test, "meow", "cat");
   print_list(test);
   printf("\n");
+  printf("Inserting ???? by fox\n");
   test = insert_front(test, "????", "fox");
   print_list(test);
+  printf("\n");
+
+  printf("========================================================\n");
+
+  printf("Testing find node: \n");
+
   return 0;
 }
